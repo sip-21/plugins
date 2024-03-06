07c7bdbed8812f392fee8a8f81eeb91241f72962
sip21 <sip21@proton.me>
2024-03-05 22:03:43 -0600

    Determine plugin dirs that have been moved out of the 'archived' into the root dir

--- .ci/test.py
+++ .ci/test.py
@@ -310,7 +310,7 @@ def run_all(args):
     print(f"plugins: {plugins}")
     if args != []:
         plugins = [p for p in plugins if p.name in args]
-        print("Testing the following plugins: {names}".format(names=[p.name for p in plugins]))
+        print("Testing the following plugins:::: {names}".format(names=[p.name for p in plugins]))
     else:
         print("Testing all plugins in {root}".format(root=root))
 
