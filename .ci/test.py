db88841096c756856221f5ead7e4d30c40b813e5
sip21 <sip21@proton.me>
2024-03-13 12:25:55 -0600

    Update test.py

--- .ci/test.py
+++ .ci/test.py
@@ -326,6 +326,8 @@ def run_all(workflow, args):
     root = Path(root_path)
 
     plugins = list(enumerate_plugins(root))
+    print("enumerate_plugins:")
+    print(enumerate_plugins)
     if args != []:
         plugins = [p for p in plugins if p.name in args]
         print("Testing the following plugins: {names}".format(names=[p.name for p in plugins]))
